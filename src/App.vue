8f223e2fb1d7b7403a9e725d609297728368a785
EndureBlaze <hi@imyan.ren>
2022-05-24 15:12:44 +0800

    🔖 0.0.1

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import { CoinbaseCommerceButton } from 'vue3-coinbase-commerce'
+// import { CoinbaseCommerceButton } from 'vue3-coinbase-commerce'
 // import 'vue3-coinbase-commerce/dist/style.css'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <CoinbaseCommerceButton :checkout-id="'75f4d64a-0073-49dc-a46b-566a50ab8325'"
+  <!-- <CoinbaseCommerceButton :checkout-id="'75f4d64a-0073-49dc-a46b-566a50ab8325'"
     >123</CoinbaseCommerceButton
-  >
+  > -->
 </template>
 
 <style>
